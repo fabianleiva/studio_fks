cd4d139ae9984e994de4dff19d26659446847f18
Fabián Leiva <fabianleivacortes@gmail.com>
2025-08-01 19:46:14 -0400

    adjust contact info

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -2,31 +2,31 @@ import { FaInstagram } from "react-icons/fa";
 
 const Contact = () => {
   return (
-    <main className="absolute inset-0 flex justify-center overflow-hidden px-3  font-work-sans">
-      {/* <div className="absolute top-6 left-1/2 -translate-x-1/2 font-semibold text-xl ">
-        <p className="text-shadow-lg">STUDIO FKS</p>
-      </div>
-      <section className="text-center absolute top-24 left-1/2 -translate-x-1/2 uppercase">
+    <main className="absolute inset-0 flex justify-center items-center overflow-hidden px-3 font-work-sans page-fade-in">
+      <section className="text-center">
         <div>
           <p>Malaga 254, oficina 52</p>
           <p>Las Condes, Santiago. CHILE</p>
           <p>Zip 7510077</p>
+          <p className="mt-5">+56 9 6835 4322</p>
+          <p>+56 9 6727 5050</p>
+          <p>+56 9 7969 8916</p>
         </div>
-        <div className="mt-0">
-          <p>fksarquitectos@gmail.cl</p>
+        <div className="mt-5">
+          <p>fksarquitectos@gmail.com</p>
           <div className="flex justify-center">
             <a
-              href="https://www.instagram.com/fks_aa/"
+              href="https://www.instagram.com/studiofks_/"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center text-[#adadad] hover:text-[#242424]"
+              className="flex items-center text-[#adadad] hover:text-[#000000]"
             >
               <FaInstagram className="text-lg mr-1" />
               fks_aa
             </a>
           </div>
         </div>
-      </section> */}
+      </section>
     </main>
   );
 };
